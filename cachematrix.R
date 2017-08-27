b27fd7b51f0771a350b2684205e3063460041fa2
hubgitark <arket@outlook.com>
2017-08-27 18:42:06 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The CACHEMATRIX darkness, which whole point being having a global "x"
 
-## Write a short comment describing this function
-
+# a matrix object that cache its inverse with sort of an API
+# no dictionary function
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get, 
+         setInverse = setInverse,
+         getInverse = getInverse)
+  }
 
-}
 
-
-## Write a short comment describing this function
-
+# Here cacheSolve takes input from makeCacheMatrix as an argument
+# and returns to it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Return a matrix that is the inverse of the "x" matrix above
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  oriX <- x$get() #the original "x" matrix
+  inv <- solve(oriX)
+  x$setInverse(inv)
+  inv
 }
+
